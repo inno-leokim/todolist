ed571e2db3cd1124171787e24b8f3e241e54e50b
inno-leokim <inno.leokim@gmail.com>
2024-01-04 17:31:03 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,107 @@
+<template>
+    <div id="app">
+        <section class="totoapp">
+            <Header @insertTodo="insertTodo" />
+            <Todo :todos="filteredList" 
+                @removeTodo="removeTodo"
+                @updateDone="updateDone"
+                @updateTodo="updateTodo"
+            />
+            <Footer 
+                :filterType="filterType" 
+                :size="filteredList.length"
+                @onFilterType="handlefilterType"
+            />
+        </section>
+    </div>
+</template>
+
+<script>
+import "./assets/css/main.css";
+
+import Header from './components/Header.vue';
+import Todo from "./components/Todo.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+    data(){
+        return {
+            todos: [
+                {
+                    id: new Date(),
+                    text: "Vue 테스트하기",
+                    isDone: true
+                },
+                {
+                    id: new Date() + 1,
+                    text: "spring 테스트하기",
+                    isDone: false
+                }
+            ],
+            filterType: 'All'
+        }
+    },
+    components: {
+        Header,
+        Todo,
+        Footer      
+    },
+    methods: {
+        insertTodo(text) {
+            this.todos = [
+                ...this.todos,
+                {
+                    id: new Date().getTime(),
+                    text,
+                    isDone: false
+                }
+            ]
+        },
+        removeTodo(id) {
+            this.todos = this.todos.filter(todo => todo.id !== id);
+        },
+        updateDone(id) {
+            const todos = [...this.todos];
+            const todo = todos.find(todo => todo.id === id);
+
+            if (todo){
+                todo.isDone = !todo.isDone;
+                this.todos = todos;
+            }
+        },
+        updateTodo({id, text}) {
+            const todos = [...this.todos];
+            const todo = todos.find(todo => todo.id === id);
+
+            if(todo) {
+                todo.text = text;
+                this.todos = todos;
+            }
+        },
+        handlefilterType(type) {
+            this.filterType = type
+        }
+    },
+    computed: {
+        filteredList() {
+            switch(this.filterType){
+                case 'ALL': {
+                    return this.todos;
+                }
+                case "Active": {
+                    return this.todos.filter((todo) => !todo.isDone);
+                }
+                case "Completed": {
+                    return this.todos.filter((todo) => todo.isDone);
+                }
+                default: {
+                    return this.todos;
+                }
+            }
+        }
+    }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+    <header class="header">
+        <h1>todos</h1>
+        <input 
+            type="text" 
+            class="new-todo" 
+            autofocus 
+            autocomplete="off" 
+            placeholder="What needs to be done?"
+            v-model="text"
+            @keyup.enter="handleTodo"
+        >
+    </header>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                text: ""
+            }
+        },
+        methods: {
+            handleTodo() {
+                const { text } = this;
+                // console.log(keycode)
+                // if(keycode === 13 && text !== ""){
+                //     this.$emit("insertTodo", text);
+                //     this.text="";
+                // }
+                this.$emit("insertTodo", text);
+                this.text="";
+                
+            }
+        }
+    }
+</script>
+
+<style scoped>
+h1 {
+  position: absolute;
+  top: 0px;
+  width: 100%;
+  font-size: 100px;
+  font-weight: 100;
+  text-align: center;
+  color: rgba(175, 47, 47, 0.15);
+  -webkit-text-rendering: optimizeLegibility;
+  -moz-text-rendering: optimizeLegibility;
+  text-rendering: optimizeLegibility;
+}
+</style>

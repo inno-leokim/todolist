998ee106f6a97ec36ff50bed2cb355bb8edaba5b
inno-leokim <inno.leokim@gmail.com>
2024-01-09 15:42:23 +0900

    EDIT add devServer, configureWebpack in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path');
+const { config } = require('process');
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
+  devServer: {
+    open: true  
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.join(__dirname, 'src/')
+      }
+    }
+  }
 })
